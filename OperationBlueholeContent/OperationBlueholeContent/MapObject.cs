69d80b223d77a974e866c2c62becee718ff16c23
wooq <wooq@nhnnext.org>
2014-10-17 19:45:37 +0900

    누락 파일 첨부

---
+++ OperationBlueholeContent/OperationBlueholeContent/MapObject.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperationBlueholeContent
+{
+    enum MapObjectId
+    {
+        VOID = 0,
+        TILE,
+        WALL,
+    }
+
+    // 기본적으로 그냥 바닥, 벽, 아무것도 아닌 것이 있는데, ID 보고 판단
+    // TILE인 경우에는 gameObject가 null인지 보고, 아니면 해당 오브젝트를 렌더?
+    class MapObject
+    {
+        public MapObjectId objectId;
+        public GameObject gameObject;
+
+        public MapObject( MapObjectId id, GameObject obj )
+        {
+            this.objectId = id;
+            this.gameObject = obj;
+        }
+    }
+}
