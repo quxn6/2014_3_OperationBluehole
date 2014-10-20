3089faca1abbc4baa80782ebb427ed9e167bfdda
1기 조익성 <slowt@naver.com>
2014-10-20 17:52:21 +0900

    파일 하나 빠짐;

---
+++ OperationBlueholeContent/OperationBlueholeContent/Party.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperationBlueholeContent
+{
+	// 파티원들의 목록과 차후 아이템 분배라던가의 속성도 넣어야 할지도?
+    class Party
+    {
+        public List<Character> mCharacters = new List<Character>();
+
+        public void AddCharacter(Character newMember)
+        {
+            mCharacters.Add(newMember);
+        }
+    }
+}
