f7b9399076dbf082c4bb743111a13052f22430fb
wooq <wooq@nhnnext.org>
2014-10-21 16:30:05 +0900

    아이템이 빠졌네

---
+++ OperationBlueholeContent/OperationBlueholeContent/Item.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperationBlueholeContent
+{
+    class Item : GameObject
+    {
+    }
+
+    // 이걸 획득하면 게임 종료
+    class RingOfErrethAkbe : Item
+    {
+
+    }
+}
