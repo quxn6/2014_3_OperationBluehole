81ecea66c640155d09129242b3d18c84aec420bc
wooq <wooq@nhnnext.org>
2014-11-06 17:04:29 +0900

    Nancy + Couchbase

---
+++ OperationBlueholeServer/OperationBlueholeServer/MatchingModule.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using Couchbase;
+using Enyim.Caching.Memcached;
+using Newtonsoft.Json;
+
+namespace OperationBlueholeServer
+{
+    using Nancy;
+
+    public static class CouchbaseManager
+    {
+        private readonly static CouchbaseClient _instance;
+
+        static CouchbaseManager()
+        {
+            _instance = new CouchbaseClient();
+        }
+
+        public static CouchbaseClient Instance { get { return _instance; } }
+    }
+
+    public class TestModule : NancyModule
+    {
+        public TestModule()
+        {
+            Get["/couchbase/"] = parameters =>
+            {
+                var client = CouchbaseManager.Instance;
+
+                var savedBeer = client.Get( "new_holland_brewing_company-sundog" );
+
+                return savedBeer;
+            };
+        }
+    }
+
+    public class MatchingModule
+    {
+    }
+}
