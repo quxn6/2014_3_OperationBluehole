96264a991556fad60af3361f89377b82c7fad344
SamKim <quxn6@naver.com>
2014-11-18 20:01:01 +0900

    man menu , ui 작업 중

---
+++ OBClient/Assets/_Scripts/Object/UserStatus.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UserStatus : MonoBehaviour
+{
+	void Update()
+	{
+		Vector3 charactorToCamera = Camera.main.transform.position - gameObject.transform.position;
+		transform.rotation = Quaternion.Euler( new Vector3( 0.0f , -160.0f + Mathf.Rad2Deg * Mathf.Atan( charactorToCamera.x / charactorToCamera.z ) , 0.0f ) );
+	}
+}
---
+++ OBClient/Assets/_Scripts/Scene/MainMenu.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour 
+{
+	public GameObject listObject = null;
+	public GameObject floatingMenu = null;	
+
+	public void SelectDungeon()
+	{
+
+	}
+
+	public void ReplayJournal()
+	{
+
+	}
+
+	public void BackButton()
+	{
+
+	}
+
+	public void MaintainCharacter()
+	{
+
+	}
+
+	public void ChangeItem()
+	{
+
+	}
+
+
+
+}
